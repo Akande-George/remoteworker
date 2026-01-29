0f15e13c6f3b3eafe9e30b05e20783d86de2d68e
Akande George <48625249+Akande-George@users.noreply.github.com>
2026-01-29 16:06:16 +0100

    mobile

--- components/reusables/Welcome.tsx
+++ components/reusables/Welcome.tsx
@@ -1,18 +1,25 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import { Button } from "../ui/button";
+import { useUserData } from "@/hooks/userData";
 
 const Welcome = () => {
+  const { data, isLoading } = useUserData();
+
   return (
     <div className="p-5 rounded-[16px] flex justify-between items-center bg-gradient-to-r from-[#FFF3F9] to-[#FFFFFF]">
       <div className="flex justify-start items-center gap-4">
         <div>
           <div className="mori-semibold text-[24px] text-[#161A21]">
-            Welcome back! Adeife 👋
+            {isLoading
+              ? "Welcome back!"
+              : `Welcome back! ${data?.first_name ?? ""} 👋`}
           </div>
           <div className="text-[14px] text-[#161A21]">
-            Your goal: Land a 
-            <span className="mori-semibold">Product Designer</span> role at 
+            {/* You can fetch and display user goal/role/salary here if available in backend */}
+            Your goal: Land a{" "}
+            <span className="mori-semibold">Product Designer</span> role at{" "}
             <span className="text-[#E9358F] mori-semibold">₦200K/month</span>
           </div>
         </div>
